9306ca0dfe38b92b19a3a0836ee55b2b22dc96c7
Frederick Clay <fredfmclay@gmail.com>
2022-12-07 00:30:26 +0000

    added lesson render for tables

--- front-end/src/components/extras/LessonPrinter.jsx
+++ front-end/src/components/extras/LessonPrinter.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 const LessonPrinter = ( {lessonContent, lessonExiter} ) => {
 
-    const {title, mainBody} = lessonContent;
+    const {title, language, mainBody} = lessonContent;
     const [content, setContent] = useState(mainBody);
 
     useEffect(() => {
@@ -25,6 +25,12 @@ const LessonPrinter = ( {lessonContent, lessonExiter} ) => {
                 case "e":
                     thisComponent = makeExample(text);
                     break;
+                case "l":
+                    thisComponent = makeList(text, element[2]);
+                    break;
+                case "c":
+                    thisComponent = makeSubPara(text);
+                    break;
                 default:
                     thisComponent = null;
             }
@@ -41,6 +47,21 @@ const LessonPrinter = ( {lessonContent, lessonExiter} ) => {
 
     const makeExample = (input) => {
         return <div className={style.ExampleBlock}>{input.split("/").map(ex => <h4>{ex}</h4>)}</div>
+    }
+
+    const makeList = (leftCol, rightCol) => {
+        const lSplit = leftCol.split("|");
+        const rSplit = rightCol.split("|");
+
+        return <table>
+            <tr><th>English</th><th>{language.charAt(0).toUpperCase() + language.slice(1)}</th></tr>
+            {rSplit.map((rSide, index) => <tr><td>{rSide}</td><td>{lSplit[index]}</td></tr>)}
+        </table>
+
+    }
+
+    const makeSubPara = (input) => {
+        return <h4>{input}</h4>
     }
 
     return (
